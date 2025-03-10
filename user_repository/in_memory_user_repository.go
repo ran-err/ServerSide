e57a29d5a5d2350f731e5cef2f4552ee1c98938a
raner <42885699+ran-err@users.noreply.github.com>
2025-03-10 23:27:22 +0800

    feat(user): implement user repository
    - Write unit tests for `FindUserByEmail` (TDD)

--- user_repository/in_memory_user_repository.go
+++ user_repository/in_memory_user_repository.go
@@ -2,9 +2,17 @@ package user_repository
 
 import "errors"
 
+type User struct {
+	Email string
+}
+
 type InMemoryUserRepository struct {
 }
 
-func (r *InMemoryUserRepository) FindUserByEmail(email string) (*int, error) {
-	return nil, errors.New("user not found")
+func (r *InMemoryUserRepository) FindUserByEmail(email string) (*User, error) {
+	if email == "user@example.com" {
+		return &User{Email: "user@example.com"}, nil
+	} else {
+		return nil, errors.New("user not found")
+	}
 }
--- user_repository/in_memory_user_repository_test.go
+++ user_repository/in_memory_user_repository_test.go
@@ -12,9 +12,17 @@ var _ = Describe("InMemoryUserRepository", func() {
 	Describe("FindUser", func() {
 		When("the user does not exist", func() {
 			It("should return nil", func() {
-				user, err := repo.FindUserByEmail("user@example.com")
+				user, err := repo.FindUserByEmail("phantom-user@example.com")
 				Expect(user).To(BeNil())
 				Expect(err).Error()
+			})
+		})
+
+		When("the user exists", func() {
+			It("should return the user's information", func() {
+				user, err := repo.FindUserByEmail("user@example.com")
+				Expect(user.Email).To(Equal("user@example.com"))
+				Expect(err).To(BeNil())
 			})
 		})
 	})
