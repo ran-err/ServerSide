86517b1ebba9f2bf61469f65796c940d728514f8
raner <42885699+ran-err@users.noreply.github.com>
2025-03-11 20:39:13 +0800

    feat(user): store users in a slice and add `ID` and `Active` fields

--- user_repository/in_memory_user_repository.go
+++ user_repository/in_memory_user_repository.go
@@ -3,10 +3,17 @@ package user_repository
 import "errors"
 
 type User struct {
-	Email string
+	ID     int
+	Active bool
+	Email  string
 }
 
+// InMemoryUserRepository stores a slice of Users by value to:
+// 1. Optimize cache efficiency.
+// 2. Prevent unintended modifications.
+// 3. Handle frequently accessed small structs efficiently.
 type InMemoryUserRepository struct {
+	Users []User
 }
 
 func (r *InMemoryUserRepository) FindUserByEmail(email string) (*User, error) {
